ba78c0958eb07fbaf5e821ea944eeb9df675fc25
Gavin Distel <gdistel@498robotics.org>
2025-07-09 10:20:20 -0400

    filled out project page, added back arrow for project page 1

--- rainbow.js
+++ rainbow.js
@@ -1,30 +1,30 @@
-console.log("Hello World!")
+// console.log("Hello World!")
 
-let element = document.getElementById("tswcontainer")
-let input = document.getElementById("my-input")
-let angle = 0;
-let shouldrainbow = true;
+// let element = document.getElementById("tswcontainer")
+// let input = document.getElementById("my-input")
+// let angle = 0;
+// let shouldrainbow = true;
 
-function onFrame() {
-    angle += 1
-    let string = "hsl(" + angle + ", 100%, 50%)"
+// function onFrame() {
+//     angle += 1
+//     let string = "hsl(" + angle + ", 100%, 50%)"
     
-    if(shouldrainbow) {
-        element.style.backgroundColor = string 
-    }
-    requestAnimationFrame(onFrame)
-}
+//     if(shouldrainbow) {
+//         element.style.backgroundColor = string 
+//     }
+//     requestAnimationFrame(onFrame)
+// }
 
-onFrame()
+// onFrame()
 
-function start_rainbow() {
-    shouldrainbow = true;
-}
+// function start_rainbow() {
+//     shouldrainbow = true;
+// }
 
-function onlmbclick(event) {
-    console.log(input.value)
-    shouldrainbow = false;
-    element.style.backgroundColor = input.value
-    setTimeout(start_rainbow,1000)
+// function onlmbclick(event) {
+//     console.log(input.value)
+//     shouldrainbow = false;
+//     element.style.backgroundColor = input.value
+//     setTimeout(start_rainbow,1000)
 
-}
+// }
